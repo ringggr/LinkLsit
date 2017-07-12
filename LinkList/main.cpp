e9b4dda42cef1e77fcdb9eb95bd862383da35bcf
Jacky <ringggr@icloud.com>
2017-07-12 15:00:42 +0800

    Initial Commit

---
+++ LinkList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkList
+//
+//  Created by lanouhn on 17/7/12.
+//  Copyright © 2017年 JackyHou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
